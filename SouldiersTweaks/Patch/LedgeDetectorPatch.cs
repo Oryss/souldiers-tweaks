f6f4435a68ee68c970fdd8d5e616e3bdee812484
Boris Baskovec <43440732+Oryss@users.noreply.github.com>
2022-06-16 21:30:39 +0200

    Add ledge grab tweak

---
+++ SouldiersTweaks/Patch/LedgeDetectorPatch.cs
@@ -0,0 +1,24 @@
+﻿using BattleDefs;
+using HarmonyLib;
+using System;
+
+namespace SouldiersTweaks.Patch
+{
+    [HarmonyPatch(typeof(LedgeDetector), "OnTriggerEnter2D")]
+    class LedgeDetectorPatch
+	{
+        public static bool Prefix()
+        {
+            var tweak = (CriticalHitBulletTimeTweak)Tweaks.GetPatchTweak(typeof(CriticalHitBulletTimeTweak));
+            if (tweak.Active)
+            {
+                if (!InputManager.s_cInstance.GetInputStatus(InputManager.InputCommand.UP).Pressed && !InputManager.s_cInstance.GetInputStatus(InputManager.InputCommand.UP).Held)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ SouldiersTweaks/Tweak/All/LedgeGrabTweak.cs
@@ -0,0 +1,22 @@
+﻿using SouldiersTweaks.Patch;
+
+namespace SouldiersTweaks
+{
+    public class LedgeGrabTweak : ToggleTweak, IPatchTweak
+    {
+        public LedgeGrabTweak() : base("Disable automatic ledge grab")
+        {
+            Active = false;
+        }
+
+        public override void OnActivate()
+        {
+            Active = true;
+        }
+
+        public override void OnDeactivate()
+        {
+            Active = false;
+        }
+    }
+}
